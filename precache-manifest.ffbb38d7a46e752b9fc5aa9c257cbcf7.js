25b9ac1f1c71ddacde5b4f59a5a143be38de0a02
OCTAU Alexandre <alexandre@octau.me>
2019-04-21 18:50:10 +0200

    Updates

---
+++ precache-manifest.ffbb38d7a46e752b9fc5aa9c257cbcf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8259d9b1795759b8dd9",
+    "url": "/static/js/main.e1324a76.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d8259d9b1795759b8dd9",
+    "url": "/static/css/main.3ce31f5e.chunk.css"
+  },
+  {
+    "revision": "54ae6bb60c2a393cb14c983b3434a62e",
+    "url": "/index.html"
+  }
+];
