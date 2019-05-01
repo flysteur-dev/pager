8d36f34af7868350e25237c577b29deb814cb29a
OCTAU Alexandre <alexandre@octau.me>
2019-05-01 18:18:36 +0200

    Adding instruction for developing/testing without the docker image

--- src/Helpers/Constants.js
+++ src/Helpers/Constants.js
@@ -1,2 +1,5 @@
-export const PROXY_PATH = '/proxy?url=';
+// During Development or without using the docker image
+// You can simply use "https://cors-anywhere.herokuapp.com/" as PROXY_PATH variable to get around CORS security.
+export const PROXY_PATH       = '/proxy?url=';
+
 export const FAVICON_PROVIDER = 'https://s2.googleusercontent.com/s2/favicons?domain_url=';
