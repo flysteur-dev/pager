b3b9b38b0480494fb850a4186ab484f2d03a6180
OCTAU Alexandre <alexandre@octau.me>
2019-05-08 01:21:15 +0200

    Updates

---
+++ precache-manifest.a632fceae9fb0c7cca4dfcfca9104911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "000e4e20a08ec0256688",
+    "url": "./static/js/main.c1a20832.chunk.js"
+  },
+  {
+    "revision": "bb033c2f99e4207645ca",
+    "url": "./static/js/2.3893f9f0.chunk.js"
+  },
+  {
+    "revision": "000e4e20a08ec0256688",
+    "url": "./static/css/main.f129675c.chunk.css"
+  },
+  {
+    "revision": "954149f73d002511fb748cdedbf4f447",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.12d9087936b54f5cffd6afa9612195a0.js"
+  "./precache-manifest.a632fceae9fb0c7cca4dfcfca9104911.js"
 );
 
 workbox.clientsClaim();
