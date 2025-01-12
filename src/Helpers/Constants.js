6cf0375991160e00e085e6f865dbc981c1aa44e5
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2025-01-12 20:44:14 +0100

    Update Constants.js

--- src/Helpers/Constants.js
+++ src/Helpers/Constants.js
@@ -1,6 +1,7 @@
 // During Development or without using the docker image
-// You can simply use "https://cors-anywhere.herokuapp.com/" as PROXY_PATH variable to get around CORS security.
-export const PROXY_PATH       = '/proxy?url=';
+// You can simply use "https://corsproxy.io/?url=" OR "https://api.allorigins.win/get?url=" as PROXY_PATH variable to get around CORS security.
+//export const PROXY_PATH       = '/proxy?url=';
+export const PROXY_PATH       = 'https://corsproxy.io/?url=';
 
 export const FAVICON_PROVIDER = 'https://s2.googleusercontent.com/s2/favicons?domain_url=';
 export const ONBOARDING_FEED  = 'https://xkcd.com/rss.xml';
